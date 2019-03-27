0e68227b3f3e10268956247d2145c47799248ce1
Taras Kvasnytsya <taras1kvasnytsya@gmail.com>
2019-03-27 23:03:42 +0200

    Add control commands (Add)

---
+++ Cinic_DBFirst_demo/Operations.cs
@@ -0,0 +1,87 @@
+﻿using Cinic_DBFirst_demo.DAL;
+using Cinic_DBFirst_demo.Models;
+using System;
+
+namespace Cinic_DBFirst_demo
+{
+    public class Operations
+    {
+        UnitOfWork unitOfWork;
+
+        public Operations(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void CreateNewDoctor()
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter information about doctor: ");
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine().Trim();
+            Console.WriteLine("Surname: ");
+            string surname = Console.ReadLine().Trim();
+            Console.WriteLine("Specialization: ");
+            string specialization = Console.ReadLine().Trim();
+
+            var doctorItem = new Doctor()
+            {
+                Name = name,
+                Surname = surname,
+                Specialization = specialization
+            };
+
+            unitOfWork.Doctors.Add(doctorItem);
+            unitOfWork.Save();
+
+            Console.WriteLine("Operation completed");
+        }
+
+        public void CreateNewPatient()
+        {
+
+        }
+
+        public void RemoveDoctor()
+        {
+
+        }
+        public void RemovePatient()
+        {
+
+        }
+        public void GetDoctor()
+        {
+
+        }
+        public void GetPatient()
+        {
+
+        }
+        public void GeAllDoctors()
+        {
+
+        }
+        public void GetAllPatients()
+        {
+
+        }
+
+        public void ShowOperations()
+        {
+            Console.Clear();
+            Console.WriteLine("Please select one operation from list:");
+            Console.WriteLine("1. Create New Doctor");
+            Console.WriteLine("2. Create New Patient");
+            Console.WriteLine("3. Remove Doctor");
+            Console.WriteLine("4. Remove Patient");
+            Console.WriteLine("5. Get Doctor");
+            Console.WriteLine("6. Get Patient");
+            Console.WriteLine("7. Show all Doctor");
+            Console.WriteLine("8. Show all Patient");
+            Console.WriteLine("Enter 'q' to quit the program");
+            Console.WriteLine("Please enter number of operation: ");
+        }
+
+    }
+}
--- Cinic_DBFirst_demo/Program.cs
+++ Cinic_DBFirst_demo/Program.cs
@@ -1,12 +1,88 @@
-﻿using System;
+﻿using Cinic_DBFirst_demo.DAL;
+using Cinic_DBFirst_demo.Models;
+using System;
 
 namespace Cinic_DBFirst_demo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var operation = new Operations(new UnitOfWork(new ClinicContext()));
+
+            while (true)
+            {
+                operation.ShowOperations();
+                string numberOfOperation = Console.ReadLine();
+                bool success = Int32.TryParse(numberOfOperation, out int number);
+                if (!success && numberOfOperation[0]!='q')
+                {
+                    Console.WriteLine("You enter an invalid number of operation, please enter correct number!");
+                    Console.ReadKey();
+                    continue;
+                }
+                if (numberOfOperation[0]=='q') 
+                {
+                    break;
+                }
+
+                switch (number)
+                {
+                    case 1:
+                        {
+                            operation.CreateNewDoctor();
+                            break;
+                        }
+                    case 2:
+                        {
+                            operation.CreateNewPatient();
+                            break;
+                        }
+                    case 3:
+                        {
+                            operation.RemoveDoctor();
+                            break;
+                        }
+                    case 4:
+                        {
+                            operation.RemovePatient();
+                            break;
+                        }
+                    case 5:
+                        {
+                            operation.GetDoctor();
+                            break;
+                        }
+                    case 6:
+                        {
+                            operation.GetPatient();
+                            break;
+                        }
+                    case 7:
+                        {
+                            operation.GeAllDoctors();
+                            break;
+                        }
+                    case 8:
+                        {
+                            operation.GetAllPatients();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("You enter an invalid number of operation, please enter correct number!");
+                            Console.ReadKey();
+                            continue;                            
+                        }                       
+                }
+
+                Console.ReadKey();
+                operation.ShowOperations();
+            }
+            Console.WriteLine("You quit a program, thanks!");
+            Console.ReadKey();
         }
+
+        
     }
 }
